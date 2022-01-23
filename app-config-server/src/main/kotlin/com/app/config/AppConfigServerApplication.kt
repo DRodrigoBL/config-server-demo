7c9c73aec228e331780c11201120545436089e59
Rodrigo Bautista <rodrigo.bautista@rappi.com>
2022-01-22 22:16:15 -0600

    config client setup completed

--- app-config-server/src/main/kotlin/com/app/config/AppConfigServerApplication.kt
+++ app-config-server/src/main/kotlin/com/app/config/AppConfigServerApplication.kt
@@ -2,9 +2,11 @@ package com.app.config
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient
 import org.springframework.cloud.config.server.EnableConfigServer
 
 @SpringBootApplication
+@EnableDiscoveryClient
 @EnableConfigServer
 class AppConfigServerApplication
 
--- authors-service/src/main/kotlin/com/app/authors/AuthorsServiceApplication.kt
+++ authors-service/src/main/kotlin/com/app/authors/AuthorsServiceApplication.kt
@@ -1,11 +1,27 @@
 package com.app.authors
 
+import org.springframework.beans.factory.annotation.Value
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RestController
 
 @SpringBootApplication
+@EnableDiscoveryClient
 class AuthorsServiceApplication
 
 fun main(args: Array<String>) {
 	runApplication<AuthorsServiceApplication>(*args)
+}
+
+
+@RestController
+class MyController(@Value("\${authors.platform-classification.gold}") val goldAuthors: String){
+
+	@GetMapping("/api/authors/gold")
+	fun retrieveGoldAuthros(): String{
+		return goldAuthors
+	}
+
 }
