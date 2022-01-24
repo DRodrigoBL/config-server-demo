665f5a53562452d1b6fdc088e4f3c74fb0a6a703
Rodrigo Bautista <rodrigo.bautista@rappi.com>
2022-01-23 19:34:32 -0600

    eureka config removed

--- authors-service/src/main/kotlin/com/app/authors/AuthorsServiceApplication.kt
+++ authors-service/src/main/kotlin/com/app/authors/AuthorsServiceApplication.kt
@@ -3,14 +3,10 @@ package com.app.authors
 import org.springframework.beans.factory.annotation.Value
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RestController
 
 @SpringBootApplication
-@EnableDiscoveryClient
-//@EnableEurekaClient
 class AuthorsServiceApplication
 
 fun main(args: Array<String>) {
