9d2efcca6169eb254da0742acf2a02956fd6dddb
Rodrigo Bautista <rodrigo.bautista@rappi.com>
2022-01-23 20:56:34 -0600

    spring cloud bus setup completed

--- authors-service/build.gradle.kts
+++ authors-service/build.gradle.kts
@@ -22,6 +22,7 @@ dependencies {
 	implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
 	implementation("org.springframework.boot:spring-boot-starter-actuator")
 	implementation("org.springframework.cloud:spring-cloud-starter-config")
+	implementation("org.springframework.cloud:spring-cloud-starter-bus-kafka")
 	implementation("org.springframework.cloud:spring-cloud-starter-bootstrap")
 	testImplementation("org.springframework.boot:spring-boot-starter-test")
 }
