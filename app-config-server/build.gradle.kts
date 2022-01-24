c834f412975eb5d13137ca269546a193282272be
Rodrigo Bautista <rodrigo.bautista@rappi.com>
2022-01-23 22:20:59 -0600

    cloud bus and cloud-config-monitor endpoint setup completed

--- app-config-server/build.gradle.kts
+++ app-config-server/build.gradle.kts
@@ -23,6 +23,7 @@ dependencies {
 	implementation("org.springframework.cloud:spring-cloud-config-server")
 	implementation("org.springframework.boot:spring-boot-starter-actuator")
 	implementation("org.springframework.cloud:spring-cloud-starter-bus-kafka")
+	implementation("org.springframework.cloud:spring-cloud-config-monitor")
 	testImplementation("org.springframework.boot:spring-boot-starter-test")
 }
 
--- authors-service/src/main/kotlin/com/app/authors/AuthorsServiceApplication.kt
+++ authors-service/src/main/kotlin/com/app/authors/AuthorsServiceApplication.kt
@@ -3,6 +3,7 @@ package com.app.authors
 import org.springframework.beans.factory.annotation.Value
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.cloud.context.config.annotation.RefreshScope
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RestController
 
@@ -15,6 +16,7 @@ fun main(args: Array<String>) {
 
 
 @RestController
+@RefreshScope
 class MyController(@Value("\${authors.platform-classification.gold:oreilly-default}") val goldAuthors: String){
 
 	@GetMapping("/api/authors/gold")
